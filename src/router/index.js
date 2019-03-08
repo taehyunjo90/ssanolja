7dfa13c4258de6dae72699e04cde62494f062d66
minjae <alswo2722@naver.com>
2019-03-08 19:53:33 +0900

    메인파일 생성

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,16 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Main from '@/views/Main.vue'
 
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
-  ]
+    mode: 'history',
+    routes: [
+        {
+            path: '/',
+            name: 'Main',
+            component: Main
+        }
+    ]
 })
